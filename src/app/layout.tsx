623464e1028dd2fc88c9c600b61a4c64f91f0829
zecajose <140219084+zecajose@users.noreply.github.com>
2025-11-10 20:18:20 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <head>
-        {/* Hotjar Tracking Code for https://hypelojas.vercel.app/ */}
+        {/* Hotjar Tracking Code for Diagnóstico da Sua Loja */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(h,o,t,j,a,r){
                   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:6571727,hjsv:6};
+                  h._hjSettings={hjid:393888,hjsv:6};
                   a=o.getElementsByTagName('head')[0];
                   r=o.createElement('script');r.async=1;
                   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
