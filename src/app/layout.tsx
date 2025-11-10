077aa83ccf760d6450b0277370664399f41de7c3
zecajose <140219084+zecajose@users.noreply.github.com>
2025-11-10 20:28:00 -0300

    Update layout código certo.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             __html: `
               (function(h,o,t,j,a,r){
                   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:393888,hjsv:6};
+                  h._hjSettings={hjid:6571727,hjsv:6};
                   a=o.getElementsByTagName('head')[0];
                   r=o.createElement('script');r.async=1;
                   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
